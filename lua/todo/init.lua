1c01d21b252f792bb1e2d4cccd9676e021332fc4
crayfish <m1992steen@gmail.com>
2024-08-24 06:38:32 +0200

    testing the setup function

--- lua/todo/init.lua
+++ lua/todo/init.lua
@@ -1,3 +1,24 @@
-function PrintHelloTodo()
-	print("hellow")
+local M = {}
+
+M.setup = function()
+	print("setup todo")
+	-- TODO: get the todos from a file
 end
+
+M.add = function()
+	-- TODO: function to add a task
+end
+
+M.delete = function()
+	-- TODO: function to delete a task
+end
+
+M.complete = function()
+	-- TODO: function to complete a task
+end
+
+M.update = function()
+	-- TODO: function to update a task
+end
+
+return M
